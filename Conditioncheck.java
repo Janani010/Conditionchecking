b0e144cb56e09088464f3476760ed7722bd09ebc
Janani010 <113532819+Janani010@users.noreply.github.com>
2022-09-30 10:29:32 +0530

    Add files via upload

---
+++ Conditioncheck.java
@@ -0,0 +1,21 @@
+class Conditioncheck
+{
+public static void main(String args[])
+{
+int x = 5;
+int y = -100;
+
+boolean resultOfX = x>0;
+
+if (resultOfX == true)
+{
+System.out.println("X is positive value="+resultOfX);
+}
+else
+{
+
+System.out.println("X is negative value="+resultOfX);
+
+}
+}
+}
